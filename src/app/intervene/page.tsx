80c3704b2ae37664b6386ab498aed6c7977fddb4
MinJi Koh <36044398+spiritstone@users.noreply.github.com>
2025-06-17 22:47:40 +0900

    feat: add camera/text btn

--- src/app/intervene/page.tsx
+++ src/app/intervene/page.tsx
@@ -3,9 +3,12 @@ export const dynamic = "force-dynamic";
 
 import SimpleText from "@/components/SimpleText";
 import NameBanner from "@/components/NameBanner";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import CameraFeed from "@/components/CameraFeed";
 
 export default function IntervenePage() {
+  const [selected, setSelected] = useState<"text" | "camera" | null>(null);
+
   return (
     <main style={{ padding: "2rem", textAlign: "center" }}>
       <h1>당신은 지금 어떤 모습인가요?</h1>
@@ -14,11 +17,23 @@ export default function IntervenePage() {
         <NameBanner />
       </Suspense>
 
+      <div
+        style={{
+          marginTop: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          gap: "1rem",
+        }}
+      >
+        <button onClick={() => setSelected("text")}>텍스트 보기</button>
+        <button onClick={() => setSelected("camera")}>카메라 보기</button>
+      </div>
+
       <div style={{ marginTop: "2rem" }}>
         <Suspense>
-          {" "}
-          <SimpleText />
-        </Suspense>{" "}
+          {selected === "text" && <SimpleText />}
+          {selected === "camera" && <CameraFeed />}
+        </Suspense>
       </div>
     </main>
   );
